708a8b80b92d5ae80c881559c6f8c7b26a69a770
Pierre Météyé <meteyepg@gmail.com>
2021-05-13 10:38:00 +0200

    corrections

--- pages/bibliographie.js
+++ pages/bibliographie.js
@@ -5,18 +5,22 @@ import React from "react";
 export default function SearchResults({bibliographie}) {
   const bibliographie_list = bibliographie.all_bibliographie.data.map((el, i) => {
     return <li key={`${el}-${i}`} className="hit-item">
-      <div className="hit-item__content">
-        <h3 className="hit-item__title">{el.titre}</h3>
-        <a href={el.lien_source} target="_blank" className="hit-item__cta button empty">Voir plus</a>
-      </div>
+      <a href={el.lien_source} target="_blank">
+        <div className="hit-item__content">
+          <p className="hit-item__date">{el.date}</p>
+          <h3 className="hit-item__title">{el.titre}</h3>
+        </div>
+      </a>
     </li>
   })
   return (
     <Template classNamePage="bibliographie" pageName="Publications">
       <h1>Bibliographie</h1>
-      <ul className="bibliographie-cards">
-        {bibliographie_list}
-      </ul>
+      <div className="bibliographie-list-wrapper">
+        <ul className="bibliographie-list">
+          {bibliographie_list}
+        </ul>
+      </div>
     </Template>
   )
 }
