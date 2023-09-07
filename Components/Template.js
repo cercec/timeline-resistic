31c878025300ae05a9a270ae23ed8ddc4e927099
Pierre Météyé <meteyepg@gmail.com>
2023-09-07 18:03:31 +0200

    update packages for node 18 due to vercel updates

--- Components/Template.js
+++ Components/Template.js
@@ -22,10 +22,6 @@ export default function Template({children, classNamePage, menuActive, pageName}
     <div className={`${classNamePage === "home" && "home-page"}`}>
       <Head>
         <title>ResisTIC Timeline - {pageName}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="/logo.png"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <div className={`container ${classNamePage}`}>
         <header>
